892e758879fcc9d701cd55e8154156901a1bf565
Srinivas <thammaneni54@gmail.com>
2018-07-25 22:51:44 +0530

    Conditional statements excercise

---
+++ Practice/ConditionalExcercise.py
@@ -0,0 +1,14 @@
+def main():
+    x,y = 100,100
+
+    if(x<y):
+        st="X is less than Y"
+    elif (x == y):
+        st="X is same as Y"
+    else:
+        st="X is greater than Y"
+
+    print (st)
+
+if __name__ == "__main__":
+    main()
